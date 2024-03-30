d1b307684177bdf8207bf510f8a26749bc9d31cb
Swikar Adhikari <swikara082@gmail.com>
2024-03-30 11:11:12 +1030

    feat: 🐱 made blogs using mdx

--- src/app/components/RecentPost.tsx
+++ src/app/components/RecentPost.tsx
@@ -1,21 +1,29 @@
+import Link from "next/link";
+import getPostMetadata from "../util/getPostMetadata";
+
 export default function RecentPost() {
+  const recentPost = getPostMetadata();
+  console.log(recentPost);
+
   return (
     <div>
-      {/* Title and Date */}
-      <div className="flex  gap-5 mt-10">
-        <h1 className=" border-b-2 hover:border-red-500 border-blue-500 cursor-pointer">
-          How Youtube Damaged My Interest&apos;s
-        </h1>{" "}
-        <p className="text-zinc-400">March,29,2024</p>
-      </div>
-      {/* Description */}
-      <div className="mt-5">
-        <p>
-          Here i will talk about how my interest slowly died by watching alot of
-          youtube videos and how i stopped by doing what i want to do instead of
-          doing what youtubers want me to do.
-        </p>
-      </div>
+      {recentPost.map((post) => (
+        <div key={post.slug}>
+          {/* Title and Date */}
+          <div className="flex  gap-5 mt-10">
+            <Link href={`post/${post.slug}`}>
+              <h1 className=" border-b-2 hover:border-red-500 border-blue-500 cursor-pointer">
+                {post.title}
+              </h1>{" "}
+            </Link>
+            <p className="text-zinc-400">{post.date}</p>
+          </div>
+          {/* Description */}
+          <div className="mt-5">
+            <p>{post.subtitle}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,8 +1,25 @@
+import getPostContent from "@/app/util/getPostContent";
+import getPostMetadata from "@/app/util/getPostMetadata";
+import Markdown from "markdown-to-jsx";
+
+export const generateStaticParams = async () => {
+  const posts = getPostMetadata();
+  return posts.map((post) => {
+    slug: post.slug;
+  });
+};
+
 export default function Page({ params }: { params: { slug: string } }) {
-  console.log(params);
+  const post = getPostContent(params.slug);
   return (
-    <div>
-      <h1>My Single Post </h1>
+    <div className="mt-10">
+      <div className=" mb-5 border-b-2">
+        <h1 className=" text-4xl mb-3">{post.data.title}</h1>
+        <p className=" text-gray-400 mb-3">{post.data.date}</p>
+      </div>
+      <article className="prose lg:prose-xl prose-headings:text-white prose-blockquote:text-red-500 prose-p:text-white prose-a:text-blue-500 prose-strong:text-white prose-ul:text-red-500 prose-table:text-white prose-code:text-purple-500">
+        <Markdown>{post.content}</Markdown>
+      </article>
     </div>
   );
 }
