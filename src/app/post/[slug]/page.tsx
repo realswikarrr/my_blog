06f20e130822b37717644332028da31912f2b082
Swikar Adhikari <swikara082@gmail.com>
2024-04-01 18:54:49 +1030

    pref: checking production error

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -4,12 +4,12 @@ import getPostContent from "@/app/util/getPostContent";
 import getPostMetadata from "@/app/util/getPostMetadata";
 import Markdown from "markdown-to-jsx";
 
-export const generateStaticParams = async () => {
-  const posts = getPostMetadata();
-  return posts.map((post) => {
-    slug: post.slug;
-  });
-};
+// export const generateStaticParams = async () => {
+//   const posts = getPostMetadata();
+//   return posts.map((post) => {
+//     slug: post.slug;
+//   });
+// };
 
 export default function Page({ params }: { params: { slug: string } }) {
   console.log(params.slug);
