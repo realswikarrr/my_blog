c69e104fb749d773552184a7e5dcf5a1302f53d1
Swikar Adhikari <swikara082@gmail.com>
2024-04-01 19:34:30 +1030

    fix: production

--- src/app/util/getPostMetadata.ts
+++ src/app/util/getPostMetadata.ts
@@ -8,7 +8,7 @@ const getPostMetadata = (): PostMetadata[] => {
   const markdownPosts = files.filter((file) => file.endsWith(".md"));
 
   const posts = markdownPosts.map((fileName) => {
-    const fileContents = fs.readFileSync(`posts/${fileName}`, "utf-8");
+    const fileContents = fs.readFileSync(`blogs/${fileName}`, "utf-8");
     const matterResult = matter(fileContents);
     return {
       title: matterResult.data.title,
