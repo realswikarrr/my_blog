d008c0ab0214d10c7723708159e4f02e767dfa79
Swikar Adhikari <swikara082@gmail.com>
2024-04-01 19:22:04 +1030

    fix: production

--- src/app/util/getPostContent.ts
+++ src/app/util/getPostContent.ts
@@ -4,9 +4,11 @@ import matter from "gray-matter";
 const getPostContent = (slug: string) => {
   const folder = "posts/";
   const file = `${folder}${slug}.md`;
-  const content = fs.readFileSync(file, "utf-8");
-  const matterResult = matter(content);
-  return matterResult;
+  if (file) {
+    const content = fs.readFileSync(file, "utf-8");
+    const matterResult = matter(content);
+    return matterResult;
+  }
 };
 
 export default getPostContent;
