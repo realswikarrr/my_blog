94100b3b74fb964de6a377d1718d9d4c4d0a1ce7
Swikar Adhikari <swikara082@gmail.com>
2024-04-01 19:09:43 +1030

    fix: production

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    async rewrites() {
+        return [
+          {
+            source: '/blog/:slug*', // define the route for accessing blog posts
+            destination: '/api/blog/:slug*', // define the destination route
+          },
+        ]
+      },
+      async redirects() {
+        return [
+          {
+            source: '/blog/:slug*', // define the route for accessing blog posts
+            destination: '/api/blog/:slug*', // define the destination route
+          },
+        ]
+      },
 };
 
 export default nextConfig;
